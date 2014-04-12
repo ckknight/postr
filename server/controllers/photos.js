1b7c9c3f1b6476eedf73c6b1f2f02d4b675fc689
Cameron Knight <ckknight@gmail.com>
2014-04-12 13:18:15 -0700

    include the photos controller

---
+++ server/controllers/photos.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var route = require('koa-route'),
+  parse = require('co-busboy'),
+  fs = require('co-fs'),
+  saveTo = require('save-to'),
+  Random = require('random-js'),
+  random = new Random(Random.engines.nativeMath),
+  path = require('path'),
+  os = require('os'),
+  nodecr = require('../config/co-nodecr');
+
+exports.init = function (app) {
+  app.use(route.post('/api/upload-photo', uploadPhoto));
+};
+
+function * uploadPhoto(next) {
+  var parts = parse(this, {
+    autoFields: true
+  });
+
+  var tmpDir = path.join(os.tmpdir(), "postr");
+  try {
+    yield fs.mkdir(tmpDir);
+  } catch (e) {
+    if (e.code !== "EEXIST") {
+      throw e;
+    }
+  }
+  var count;
+  var part;
+  var filepath = null;
+  for (count = 0; part = yield parts; ++count) {
+    if (count > 1) {
+      throw new Error("Only expected one file uploaded");
+    }
+    filepath = path.join(tmpDir, random.uuid4() + path.extname(part.filename));
+    yield saveTo(part, filepath);
+  }
+  if (!filepath) {
+    throw new Error("Expected at least one file to be uploaded");
+  }
+
+  this.body = {
+    text: yield nodecr.process(filepath, 'eng', null, null, [{
+      rotate: '90deg'
+    }])
+  };
+}
